783938baf083bf95f64cd8ae8d4206fac622166d
mashirnizamikpi <mashir.nizami@kpipartners.com>
2022-10-14 20:31:50 +0530

    Commit for Assignment

--- 0.0.1-project/src/main/java/Public_Thread/Comparison.java
+++ 0.0.1-project/src/main/java/Public_Thread/Comparison.java
@@ -1,5 +1,6 @@
 package Public_Thread;
 
+import java.util.ArrayList;
 import java.util.Comparator;
 import java.util.List;
 import java.util.concurrent.TimeUnit;
@@ -7,60 +8,63 @@ import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 public class Comparison implements Runnable{
-	
-	public Integer maxInt() {
-		
-		   List<Integer> ints = Stream.of(1,2,4,3,5,9,11,15,21,22).collect(Collectors.toList());
-		    Integer maxInt = ints.stream()
-		                              .max(Comparator.comparing(i -> i))
-		                              .get();
-		   
-		     
-		    System.out.println("Greatest Number is "+maxInt);
-		    return maxInt;
-		    }
-	
 	@Override
 	public void run() {
 		// TODO Auto-generated method stub
-	System.out.println("Thread is running");
+		System.out.println("Thread is running");
 		
 	}
 
-	
-	public Integer minInt() {
-		
-		   List<Integer> ints = Stream.of(1,2,4,3,5,9,11,15,21,22).collect(Collectors.toList());
-		  
-		   
-		    Integer minInt = ints.stream()
-              .min(Comparator.comparing(i -> i))
-              .get();
-		    System.out.println("Min Number is "+minInt);
-		    
-		    return minInt;
-		    }
-	
+		Integer maxInt(List<Integer> num) {
+
+			Integer max = num.stream().max(Comparator.comparing(i -> i)).get();
+			System.out.println("Minimum number  is " + max);
+
+			return max;
+
+		}
+
+		Integer minInt(List<Integer> num) {
+
+			Integer min = num.stream().min(Comparator.comparing(i -> i)).get();
+			System.out.println("Maximum number is " + min);
+
+			return min;
+
+		}
 	public static void main(String args[]) throws InterruptedException {
 		
+
+		List<Integer> num = new ArrayList<Integer>();
+		num.add(11);
+		num.add(12);
+		num.add(13);
+		num.add(14);
+		num.add(15);
+		num.add(16);
+		num.add(17);
+		num.add(18);
+		num.add(19);
+		num.add(20);
+		
 		Runnable r1 = new Comparison(); 
-		Comparison co = new Comparison();
+		Comparison mm = new Comparison();
 		
 		Thread th1 = new Thread(r1, "My First thread");  //First Thread 
 		th1.start();
-		co.maxInt();
+		mm.maxInt(num);
 		Long duration = (long)(Math.random()*5);
 		TimeUnit.SECONDS.sleep(duration);
 		Thread th2 = new Thread(r1, "My Second thread"); //Second Thread
 		th2.start();
-		co.minInt();
-
-		    
-		   
+		
+		mm.minInt(num);
 
 		    
 
 	}
+
+
 
 
 }
