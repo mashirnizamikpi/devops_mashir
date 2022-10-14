2c927ac8f7950f919457c47805a300eb37b490d5
mashirnizamikpi <mashir.nizami@kpipartners.com>
2022-10-14 19:39:59 +0530

    Commit for Assignment

--- 0.0.1-project/src/main/java/Public_Thread/Comparison.java
+++ 0.0.1-project/src/main/java/Public_Thread/Comparison.java
@@ -2,10 +2,11 @@ package Public_Thread;
 
 import java.util.Comparator;
 import java.util.List;
+import java.util.concurrent.TimeUnit;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
-public class Comparison {
+public class Comparison implements Runnable{
 	
 	public Integer maxInt() {
 		
@@ -15,9 +16,17 @@ public class Comparison {
 		                              .get();
 		   
 		     
-		    
+		    System.out.println("Greatest Number is "+maxInt);
 		    return maxInt;
 		    }
+	
+	@Override
+	public void run() {
+		// TODO Auto-generated method stub
+	System.out.println("Thread is running");
+		
+	}
+
 	
 	public Integer minInt() {
 		
@@ -27,14 +36,23 @@ public class Comparison {
 		    Integer minInt = ints.stream()
               .min(Comparator.comparing(i -> i))
               .get();
-		    
+		    System.out.println("Min Number is "+minInt);
 		    
 		    return minInt;
 		    }
 	
-	public static void main(String args[]) {
+	public static void main(String args[]) throws InterruptedException {
+		
+		Runnable r1 = new Comparison(); 
 		Comparison co = new Comparison();
+		
+		Thread th1 = new Thread(r1, "My First thread");  //First Thread 
+		th1.start();
 		co.maxInt();
+		Long duration = (long)(Math.random()*5);
+		TimeUnit.SECONDS.sleep(duration);
+		Thread th2 = new Thread(r1, "My Second thread"); //Second Thread
+		th2.start();
 		co.minInt();
 
 		    
@@ -43,6 +61,7 @@ public class Comparison {
 		    
 
 	}
+
 
 }
 
